8b0b99974e77d754d27745959388f71a38fbee29
zhouxiankai <zhouxiankai@erwan365.com>
2020-12-10 14:06:20 +0800

    Initial commit

---
+++ src/test/java/com/alibaba/cloud/demo/aliclouddemo/AliCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.demo.aliclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
